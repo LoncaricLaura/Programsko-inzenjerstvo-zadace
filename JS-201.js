5a322bee30470d68dbe2f5714def17580673ebff
LoncaricLaura <laura.loncaric032@gmail.com>
2021-10-24 20:09:03 +0200

    JS-201.js

--- JS-201.js
+++ JS-201.js
@@ -1,8 +1,9 @@
          //JS-201
-         let x = '#';
-         for (let i = 0; i <= 5; i++) {
+         let x = '';
+         for (let i = 1; i <= 5; i++) {
              for (let j = 1; j <= i; j++) {
-                 console.log(x);
-                 x++;
+                 x += '#';
              }
+             x += '\n';
          }
+         console.log(x);
